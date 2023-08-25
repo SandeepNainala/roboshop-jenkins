aa47111062f52a61f16f60166ce21115bafe1815
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-08-25 13:03:26 +0530

    Jenkins - multi branch pipeline ansible - groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -21,12 +21,14 @@ def call() {
             }
             stage('Code Quality') {
                 steps {
-                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.89.203:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+//                    sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.89.203:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+                    sh 'echo Code Quality'
                 }
             }
             stage('Unit Test Cases') {
                 steps {
                     sh 'echo Unit Test Cases'
+                    sh 'mvn test'
                 }
             }
             stage('CheckMarx SAST Scan') {
